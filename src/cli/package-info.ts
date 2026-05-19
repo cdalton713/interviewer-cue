d778d855779bfc1a68c61896646094e479697ac4
Christian Dalton <cdalton713@gmail.com>
2026-05-18 22:22:14 -0600

    chore: release v0.2.1

--- src/cli/package-info.ts
+++ src/cli/package-info.ts
@@ -1,3 +1,3 @@
 export const PACKAGE_NAME = "interviewer-cue";
-export const PACKAGE_VERSION = "0.2.2";
+export const PACKAGE_VERSION = "0.2.1";
 export const GITHUB_REPOSITORY = "cdalton713/interviewer-cue";
